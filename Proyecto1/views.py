51463087b3ca51e8c91ffc24866e5f5fab3bb151
JoanMejiaGutierrez <l16100233@nlaredo.tecnm.mx>
2022-05-06 00:54:46 -0500

    Dar formatos a los textos con html

--- Proyecto1/views.py
+++ Proyecto1/views.py
@@ -1,7 +1,22 @@
 from django.http import HttpResponse
 
+documentoSaludo = """
+<html>
+    <body>
+        <H1>Hola, Esta es una prueba de django </H1>
+    </body>
+</html>
+"""
+documentoAdios = """
+<html>
+    <body>
+        <H1>Adios </H1>
+    </body>
+</html>
+"""
+
 def saludo(request):
-    return HttpResponse("Hola alumno esta es nuestra pagina en django")
+    return HttpResponse(documentoSaludo)
 
 def despedida(request):
-    return HttpResponse("Adios")
+    return HttpResponse(documentoAdios)
